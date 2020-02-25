b1e876f6cde23b334698a76eed2f2ecc6a2cf900
Gerson <lindorjr.0317@gmail.com>
2020-02-24 21:25:54 -0800

    select and update query is done

--- database/project_db_queries.sql
+++ database/project_db_queries.sql
@@ -1,10 +1,17 @@
 -- insert queries
 
-
 -- select queries
-
+-- Reports page: searching team name search bar
+SELECT	team_name, revenue, city, state, sport 
+FROM 	teams JOIN sports
+	ON	teams.team_id = sports.sport_id
+WHERE	team_name = :teamNameInput;
 
 -- update queries
+-- Administration page: updating a sport
+UPDATE sports 
+	SET sport_name = :sportName, professional_organization = :profOrg
+    WHERE sport_name = :sportName
 
 
 -- delete queries
