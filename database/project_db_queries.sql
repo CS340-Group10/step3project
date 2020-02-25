7ad56a7de449fb4ea6106a4bb35f9f394cfd4023
Gerson <lindorjr.0317@gmail.com>
2020-02-24 22:29:06 -0800

    final query update

--- database/project_db_queries.sql
+++ database/project_db_queries.sql
@@ -1,14 +1,14 @@
 -- insert queries
 INSERT INTO injuries (injury_type, injury_name, injury_notes)
-	VALUES ("Knee", "ACL tear", "Very common in Football")
+	VALUES ('Knee', 'ACL tear', 'Very common in Football');
 
 INSERT INTO sports (sport_name, professional_organization)
-	VALUES (:sportName, :profOrg)
+	VALUES ('Soccer', 'MLS');
 
 -- select queries
 -- Reports page: searching team name search bar
-SELECT	team_name, revenue, city, state, sport 
-FROM 	teams JOIN sports
+SELECT	team_name, revenue, city, state, sport FROM teams 
+	JOIN sports
 	ON	teams.team_id = sports.sport_id
 WHERE	team_name = :teamNameInput;
 
