22ce4d9605b19f4230d805c6208e128fac3737de
Gerson <lindorjr.0317@gmail.com>
2020-03-02 22:43:52 -0800

    Bug: get request incomplete in scripts/project.js

--- scripts/project.js
+++ scripts/project.js
@@ -2,14 +2,27 @@ var mysql = require('./dbconnect.js');
 var express = require('express');
 var bodyParser = require('body-parser');
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+// var handlebars = require('express-handlebars');
 
-const PORT = 8124;
+//const PORT = 8124;
 
 var app = express();
 // setup engine
 app.engine('handlebars', handlebars.engine);
+/************************************************/
+/*
+app.engine('handlebars', handlebars({
+	defaultLayout: 'main',
+	extname: '.handlebars',
+	layoutsDir: __dirname + '/views/layouts/'
+}));
+*/
+//app.set('views', path.join(__dirname, '/views/'));
+/*************************************************/
 app.set('view engine', 'handlebars');
-app.use(bodyParser.urlencoded({extended:true}));
+app.set('port', 8124);
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json)
 
 // POST and GET requests
 app.get('/home', function(req, res) {
@@ -20,9 +33,8 @@ app.get('/home', function(req, res) {
 
 
 // listen on port
-app.listen(PORT, function() {
-	console.log("listening on Port " + PORT + 
-		'; Enter Control-C to Exit')
+app.listen(app.get('port'), function() {
+	console.log('listening on Port 8124; Enter Control-C to Exit');
 })
 
 
