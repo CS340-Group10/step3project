e74d4877e2cb8b164024c09e24edea8e485575a7
Gerson <lindorjr.0317@gmail.com>
2020-03-02 16:26:27 -0800

    db connection done

--- scripts/dbconnect.js
+++ scripts/dbconnect.js
@@ -1,10 +1,19 @@
 var mysql = require('mysql')
 var pool = mysql.createPool({
     connectionLimit : 10,
-    host            : 'classmysql.engr.oregonstate.edu'
-    user            : 'cs340_lindorg'
-    password        :  '0983'
+    host            : 'classmysql.engr.oregonstate.edu',
+    user            : 'cs340_lindorg',
+    password        :  '0983',
     database        :  'cs340_lindorg'
-});
+}); 
 
-module.export.pool = pool;
+
+// Testing the connection:  connection pass
+/*
+pool.getConnection(function(err, connection) {
+	if (err) throw err;
+	console.log("Connected!");
+});
+*/
+
+module.exports.pool = pool;
